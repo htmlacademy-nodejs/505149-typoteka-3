37cbf66225f5cf8c96da8b66fb5b173daa2653b6
popov7 <deniolp@gmail.com>
2020-02-13 23:02:50 +0400

    Writing service

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const chalk = require(`chalk`);
+const {Cli} = require(`./cli`);
+const {
+  DEFAULT_COMMAND,
+  MAX_DATA_COUNT,
+  USER_ARGV_INDEX,
+  ExitCode
+} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+const count = userArguments.slice(1);
+if (count >= MAX_DATA_COUNT) {
+  console.error(chalk.red(`Не больше 1000 публикаций`));
+  process.exit(ExitCode.error);
+}
+
+Cli[userCommand].run(count);
