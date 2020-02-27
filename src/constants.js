c30ebb3e143ad5f430dbc516f88de2ae3bf16687
popov7 <deniolp@gmail.com>
2020-02-27 20:32:28 +0400

    Adding HttpCodes to constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,20 @@
 'use strict';
 
 module.exports.DEFAULT_COMMAND = `--help`;
+
 module.exports.MAX_DATA_COUNT = 1000;
+
 module.exports.USER_ARGV_INDEX = 2;
+
 module.exports.ExitCode = {
   error: 1,
   success: 0,
+};
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };
