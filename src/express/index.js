3db8428cb99008960066e46ad179df64b3142aa3
popov7 <deniolp@gmail.com>
2020-03-12 22:03:48 +0500

    Adding the first template

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const myRoutes = require(`./routes/my`);
 const offersRoutes = require(`./routes/offers`);
@@ -9,10 +10,13 @@ const app = express();
 const port = 8080;
 app.listen(port);
 
+app.set(`views`, path.join(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 
-app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/`, (req, res) => res.render(`main`, {}));
 app.get(`/register`, (req, res) => res.send(`/register`));
 app.get(`/login`, (req, res) => res.send(`/login`));
 app.get(`/search`, (req, res) => res.send(`/search`));
