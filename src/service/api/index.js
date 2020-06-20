a3f0c2fa1e004db588b2ce17bd36e2a318471b5f
popov7 <deniolp@gmail.com>
2020-06-20 15:36:32 +0300

    Implementing search in titles

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -4,12 +4,14 @@ const {Router} = require(`express`);
 
 const article = require(`../api/article`);
 const category = require(`../api/category`);
+const search = require(`../api/search`);
 const getMockData = require(`../lib/get-mock-data`);
 
 const {
   ArticleService,
   CommentService,
   CategoryService,
+  SearchService,
 } = require(`../data-service`);
 
 const app = new Router();
@@ -19,6 +21,7 @@ const app = new Router();
 
   article(app, new ArticleService(mockData), new CommentService());
   category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
 })();
 
 module.exports = app;
--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -3,9 +3,11 @@
 const ArticleService = require(`./article`);
 const CommentService = require(`./comment`);
 const CategoryService = require(`./category`);
+const SearchService = require(`./search`);
 
 module.exports = {
   ArticleService,
   CommentService,
   CategoryService,
+  SearchService,
 };
