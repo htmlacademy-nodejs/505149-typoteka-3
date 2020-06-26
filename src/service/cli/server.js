e63278db14eaa50c64f47bf29120ea45b44d026a
popov7 <deniolp@gmail.com>
2020-06-26 16:10:23 +0300

    Making server more abstract for tests in future

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -3,21 +3,31 @@
 const chalk = require(`chalk`);
 const express = require(`express`);
 
-const {HttpCode} = require(`../../../src/constants`);
-const routes = require(`../api`);
+const {HttpCode} = require(`../../constants`);
+const createApi = require(`../api`);
 
 const DEFAULT_PORT = 3000;
 
-const app = express();
-app.use(express.json());
-app.use(`/api`, routes);
+const createApp = async () => {
+  const app = express();
+  const apiRoutes = await createApi();
 
-app.use((req, res) => res.status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+  app.use(express.json());
+  app.use(`/api`, apiRoutes);
+
+  app.use((req, res) => {
+    return res.status(HttpCode.NOT_FOUND)
+      .send(`Not found`);
+  });
+
+  return app;
+};
 
 module.exports = {
   name: `--server`,
-  run(args) {
+  createApp,
+  async run(args) {
+    const app = await createApp();
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
