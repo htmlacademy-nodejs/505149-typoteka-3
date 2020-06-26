740dd49619236abcc0cc8bc5950d103fa5a76648
popov7 <deniolp@gmail.com>
2020-06-26 17:12:38 +0300

    Creating 2 streams of logs with pino (one is writing to files /service/logs/)

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,9 +1,29 @@
 'use strict';
 
-const logger = require(`pino`)({
-  name: `pino-in-express`,
-  level: process.env.LOG_LEVEL || `info`
+const fs = require(`fs`);
+const pinoms = require(`pino-multi-stream`);
+
+const prettyStream = pinoms.prettyStream({
+  prettyPrint: {
+    colorize: true,
+    translateTime: `dd-mm-yyyy HH:MM:ss`,
+    ignore: `hostname,pid`,
+    levelFirst: true,
+  },
+  prettifier: require(`pino-pretty`)
 });
+
+const level = process.env.LOG_LEVEL || `info`;
+const streams = [
+  {level, stream: prettyStream},
+  {level, stream: fs.createWriteStream(`./src/service/logs/all.txt`)},
+  {level: `error`, stream: fs.createWriteStream(`./src/service/logs/errors.txt`)},
+];
+
+const logger = pinoms({
+  name: `pino-in-express`,
+  level,
+}, pinoms.multistream(streams));
 
 module.exports = {
   logger,
