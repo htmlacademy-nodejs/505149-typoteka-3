29ccc7e4edf6a52ae4e20cb31bbe36ccb0ec580a
popov7 <deniolp@gmail.com>
2020-07-06 19:41:46 +0300

    Implementing of showing comments on route /my/comments

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -4,14 +4,42 @@ const {Router} = require(`express`);
 const {DateTimeFormat} = require(`intl`);
 
 const getArticles = require(`../api/articles`);
+const getArticle = require(`../api/article`);
+const getComments = require(`../api/comments`);
 
 const myRouter = new Router();
 
-myRouter.get(`/`, async (req, res) => {
-  const articles = await getArticles();
+let myArticles = null;
 
-  res.render(`my`, {articles, title: `Мои публикации`, DateTimeFormat});
+myRouter.get(`/`, async (req, res) => {
+  myArticles = await getArticles();
+
+  res.render(`my`, {myArticles, title: `Мои публикации`, DateTimeFormat});
 });
-myRouter.get(`/comments`, (req, res) => res.render(`comments`, {title: `Комментарии`}));
+
+myRouter.get(`/comments`, async (req, res) => {
+  if (!myArticles) {
+    myArticles = (await getArticles()).slice(0, 3);
+  } else {
+    myArticles = myArticles.slice(0, 3);
+  }
+
+  const articlesId = myArticles.map((it) => it.id);
+
+  const sortedByCommentDate = await Promise.all(articlesId.map((id) => getComments(id)))
+      .then((results) => {
+        return results.flat().sort((a, b) => (new Date(b.date)) - (new Date(a.date)));
+      });
+
+  const commentsWithArticleTitle = await Promise.all(sortedByCommentDate.map(async (it) => {
+    const article = await getArticle(it.articleId);
+    it.articleTitle = article.title;
+    return it;
+  })).then((results) => {
+    return results;
+  });
+
+  res.render(`comments`, {commentsWithArticleTitle, title: `Комментарии`, DateTimeFormat});
+});
 
 module.exports = myRouter;
