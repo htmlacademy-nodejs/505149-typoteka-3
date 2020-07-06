c84b6a300c2ac24efff237df0049c902aff15aa3
popov7 <deniolp@gmail.com>
2020-07-06 14:41:10 +0300

    Implementing of showing article on route /my

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const {Router} = require(`express`);
+const {DateTimeFormat} = require(`intl`);
+
+const getArticles = require(`../../api/articles`);
+
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my`, {title: `Мои публикации`}));
+myRouter.get(`/`, async (req, res) => {
+  const articles = await getArticles();
+
+  res.render(`my`, {articles, title: `Мои публикации`, DateTimeFormat});
+});
 myRouter.get(`/comments`, (req, res) => res.render(`comments`, {title: `Комментарии`}));
 
 module.exports = myRouter;
