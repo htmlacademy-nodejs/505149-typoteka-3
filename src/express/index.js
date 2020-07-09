d0ae27e1a13e2492100439c753f09393434a1beb
popov7 <deniolp@gmail.com>
2020-07-09 16:07:35 +0300

    Adding urlencoded middleware before app.listen

--- src/express/index.js
+++ src/express/index.js
@@ -7,12 +7,14 @@ const {DateTimeFormat} = require(`intl`);
 const getArticles = require(`./api/articles`);
 const myRoutes = require(`./routes/my`);
 const articlesRoutes = require(`./routes/articles`);
-const {getSortedByDateComments} = require(`../lib/sort-comments`);
+const {getSortedByDateComments} = require(`../lib/utils`);
+const {getLogger} = require(`../lib/logger`);
+
+const logger = getLogger();
 
 const app = express();
 const port = 8080;
-app.listen(port);
-
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, `files`)));
 
 app.set(`views`, path.join(__dirname, `templates`));
@@ -40,4 +42,12 @@ app.use((req, res) => {
 app.use((err, req, res, _next) => {
   res.status(err.status || 500);
   res.render(`errors/500`, {title: `Ошибка сервера`});
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return logger.error(`Ошибка при создании сервера: ${err}`);
+  }
+
+  return logger.info(`Ожидаю соединений на ${port} порт`);
 });
