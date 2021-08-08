72009b2104482835bbe520a418ae5ad53a8a63e4
deniolp <deniolp@gmail.com>
2021-08-09 00:14:33 +0300

    Refactoring /my... routes

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -5,12 +5,9 @@ const {DateTimeFormat} = require(`intl`);
 
 const {getSortedByDateComments} = require(`../../lib/utils`);
 const api = require(`../api`).getAPI();
-
-const ARTICLES_PER_PAGE = 8;
+const {ARTICLES_PER_PAGE} = require(`../../constants`);
 
 const myRouter = new Router();
-
-let myArticles = null;
 
 myRouter.get(`/`, async (req, res) => {
   let {page = 1} = req.query;
@@ -18,6 +15,7 @@ myRouter.get(`/`, async (req, res) => {
 
   const limit = ARTICLES_PER_PAGE;
   const offset = (page - 1) * ARTICLES_PER_PAGE;
+
   const {count, articles} = await api.getArticles({limit, offset});
   const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
 
@@ -31,19 +29,9 @@ myRouter.get(`/`, async (req, res) => {
 });
 
 myRouter.get(`/comments`, async (req, res) => {
-  if (!myArticles) {
-    myArticles = (await api.getArticles()).slice(0, 3);
-  } else {
-    myArticles = myArticles.slice(0, 3);
-  }
-
-  const articlesId = myArticles.map((it) => it.id);
-  const sortedByDateComments = await getSortedByDateComments(articlesId);
-  for (const comment of sortedByDateComments) {
-    comment.articleTitle = (await api.getArticle(comment[`article_id`])).title;
-  }
-
-  res.render(`comments`, {sortedByDateComments, title: `Комментарии`, DateTimeFormat});
+  const articles = await api.getArticles({comments: true});
+  const sortedByDateComments = await getSortedByDateComments(articles);
+  res.render(`comments`, {sortedByDateComments, articles, title: `Комментарии`, DateTimeFormat});
 });
 
 module.exports = myRouter;
